8f5d7555cf6b4d4bc5c70263b632d35c3c3cfb30
Sim Brar <sim.brar@hootsuite.com>
2017-09-29 16:42:34 +0100

    add basic logic for no events

--- client/src/components/Wedding/EventsView.js
+++ client/src/components/Wedding/EventsView.js
@@ -1,25 +1,47 @@
 import React from 'react';
 
-import '../../styles/HomeView.css';
+import '../../styles/EventsView.css';
 
 export default class EventsView extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  handleCreateClick() {
+    console.log('I will create an event');
+  }
+
   renderEventsList() {
-    if (this.props.weddingStore.events.length === 0) {
+    if (this.props.weddingStore.events.length > 0) {
       return (
         <div className="empty-event-list">
-          <p>You do not currently have any events scheduled. To get started, add an event.</p>
+          <p>You do not currently have any events scheduled.</p>
+          <p>To get started, add an event.</p>
         </div>
-      )
+      );
     }
+  }
+
+  renderCreateEventButton() {
+    // TODO: Make this into a button component
+    return (
+      <div className="create-event-button" onClick={this.handleCreateClick}>
+        <p>Add a new event</p>
+      </div>
+    );
   }
 
   render() {
     return (
       <div id="events-view" className="col-md-8">
         <div className="title">
-          <p>Your Scheduled Events</p>
+          <p className="description">Your Scheduled Events</p>
+          <p className="subheading">Click on an event below to view details or edit</p>
         </div>
         <div className="events-list">
+          { this.renderEventsList() }
+          { this.renderCreateEventButton() }
         </div>
       </div>
     );
